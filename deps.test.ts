9ddbee49970b083b3ab43b7a3c1a59f1ce5ff46e
fundyc <luis.navarro@gmail.com>
2020-08-14 10:15:55 +0200

    Add more test to cover all the cases

--- deps.test.ts
+++ deps.test.ts
@@ -1,3 +1,3 @@
-export { assert } from "https://deno.land/std@0.61.0/testing/asserts.ts";
+export { assertStrictEquals } from "https://deno.land/std@0.61.0/testing/asserts.ts";
 export { runBenchmarks, bench } from "https://deno.land/std@0.61.0/testing/bench.ts";
-export { soxa } from 'https://deno.land/x/soxa@v1.1/mod.ts'
+export { soxa } from 'https://deno.land/x/soxa@v1.3/mod.ts'
--- server.test.ts
+++ server.test.ts
@@ -1,24 +1,54 @@
-import { assert } from "./deps.test.ts";
+import { assertStrictEquals } from "./deps.test.ts";
 import { app } from './server.ts';
 import { runBenchmarks, bench } from "./deps.test.ts";
 import { soxa } from './deps.test.ts'
 
-Deno.test('Web config', async () => {
+Deno.test('Default dev config', async () => {
     app;
-    const res = await soxa.get('http://0.0.0.0:8080/web');
-    assert(res.data.name);
+    const res = await soxa.get('http://0.0.0.0:8080/default');
+    assertStrictEquals(res.data.type, 'default');
+    assertStrictEquals(res.data.environment, 'dev');
 })
 
-Deno.test('Web config 100 times', async () => {
+Deno.test('Web dev config', async () => {
+    app;
+    const res = await soxa.get('http://0.0.0.0:8080/web');
+    assertStrictEquals(res.data.type, 'web');
+    assertStrictEquals(res.data.environment, 'dev');
+})
+
+Deno.test('Mobile dev config', async () => {
+    app;
+    const res = await soxa.get('http://0.0.0.0:8080/mobile');
+    assertStrictEquals(res.data.type, 'mobile');
+    assertStrictEquals(res.data.environment, 'dev');
+})
+
+Deno.test('Default test config', async () => {
+    app;
+    const res = await soxa.get('http://0.0.0.0:8080/default', {headers: {'Cookie': 'env=test'}});
+    assertStrictEquals(res.data.type, 'default');
+    assertStrictEquals(res.data.environment, 'test');
+})
+
+Deno.test('Mobile test config', async () => {
+    app;
+    const res = await soxa.get('http://0.0.0.0:8080/mobile', {headers: {'Cookie': 'env=test'}});
+    assertStrictEquals(res.data.type, 'mobile');
+    assertStrictEquals(res.data.environment, 'test');
+})
+
+Deno.test('Web deb config 1000 times', async () => {
     app;
     let res;
     bench({
         name: 'call for default config 1000 times',
-        runs: 100,
+        runs: 1000,
         async func(b): Promise<void> {
             b.start();
             res = await soxa.get('http://0.0.0.0:8080/web');
-            assert(res.data.name);
+            assertStrictEquals(res.data.type, 'web');
+            assertStrictEquals(res.data.environment, 'dev');
             b.stop();
         }
     })
